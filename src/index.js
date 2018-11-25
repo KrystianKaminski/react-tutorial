b46daa1c6ad4b3b9e6fb9c878394740add73cda5
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-25 14:32:16 +0100

    Create template of tic-tac-toe game with React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+import Game from './Game'
+
+ReactDOM.render( < Game / > , document.getElementById('root'));
